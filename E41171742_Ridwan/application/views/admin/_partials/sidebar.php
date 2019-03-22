4c1ce04fcdeab680c267d92323149ca6ab081d30
Ridwanizer <44997423+Ridwanizer@users.noreply.github.com>
2019-03-22 09:49:32 +0700

    E41171742_Ridwan

---
+++ E41171742_Ridwan/application/views/admin/_partials/sidebar.php
@@ -0,0 +1,43 @@
+<ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Master</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          
+          <a class="dropdown-item" >Barang</a>
+          <a class="dropdown-item" >Karyawan</a>
+          <a class="dropdown-item" >User</a>
+          <a class="dropdown-item" >Supplier</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Transaksi</span>
+          </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          
+          <a class="dropdown-item" >Penjualan</a>
+          <a class="dropdown-item" >Pembelian</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-fw fa-table"></i>
+          <span>Laporan</span>
+          </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" >Laporan Harian</a>
+          <a class="dropdown-item" >Laporan Bulanan</a>
+          <a class="dropdown-item" >Laporan Tahunan</a>
+        </div>
+      </li>
+    </ul>
