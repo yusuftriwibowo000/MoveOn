a2ca4460760e5e5b9e3c015c70b09ffb9b29d137
Yusuf <yusuftriwibowo000@gmail.com>
2019-03-26 14:27:13 +0700

    Yusuf
    Mantap

--- E41171874_YUSUF/application/views/admin/modul template/sidebar.php
+++ E41171874_YUSUF/application/views/admin/modul template/sidebar.php
@@ -1,6 +1,6 @@
 <ul class="sidebar navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?php echo site_url('admin/Dashboard')?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span>
         </a>
